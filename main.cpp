adbe72e9d576de1c97b16f1ea5c4ad6ffcb638ed
Jesús <was121995@gmail.com>
2018-02-15 15:28:10 +0100

    rename

--- main.cpp
+++ main.cpp
@@ -162,7 +162,7 @@ void funDisplay() {
         glColor3f(0.75f,1.0f, 0.0f);
         glVertex3f(0.0f, -0.5f, -8.0f);
         
-        glColor3f(0.0f, 0.0f, 0.5f);
+        glColor3f(1.0f, 0.0f, 0.5f);
         glVertex3f(-0.5f, 0.0f, -9.0f);
         glVertex3f(-0.5f, -0.5f, -9.0f);
         glVertex3f(0.0f, -1.0f, -8.0f);
